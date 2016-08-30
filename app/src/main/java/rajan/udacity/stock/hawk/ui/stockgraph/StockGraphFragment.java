d3a5236573c8c4d033adb31488352dc7b1ca6463
Rajan Maurya <rajanmaurya154@gmail.com>
2016-08-31 00:23:40 +0530

    StockGraph Activiyt added

--- app/src/main/java/rajan/udacity/stock/hawk/ui/stockgraph/StockGraphFragment.java
+++ app/src/main/java/rajan/udacity/stock/hawk/ui/stockgraph/StockGraphFragment.java
@@ -1,7 +1,30 @@
 package rajan.udacity.stock.hawk.ui.stockgraph;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rajan.udacity.stock.hawk.util.Constants;
+
 /**
  * Created by Rajan Maurya on 30/08/16.
  */
-public class StockGraphFragment {
+public class StockGraphFragment extends Fragment {
+
+
+    public static StockGraphFragment newInstance(String symbol) {
+        StockGraphFragment fragment = new StockGraphFragment();
+        Bundle args = new Bundle();
+        args.putString(Constants.SYMBOL, symbol);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
