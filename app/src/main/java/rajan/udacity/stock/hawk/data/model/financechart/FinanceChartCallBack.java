ae54abd44becc2c0d00abe5fd3f3727a5d474ef1
Rajan Maurya <rajanmaurya154@gmail.com>
2016-08-31 01:08:24 +0530

    Models added done

--- app/src/main/java/rajan/udacity/stock/hawk/data/model/financechart/FinanceChartCallBack.java
+++ app/src/main/java/rajan/udacity/stock/hawk/data/model/financechart/FinanceChartCallBack.java
@@ -1,9 +1,103 @@
 package rajan.udacity.stock.hawk.data.model.financechart;
 
+import com.google.gson.annotations.SerializedName;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Rajan Maurya on 31/08/16.
  */
-public class FinanceChartCallBack {
+public class FinanceChartCallBack implements Parcelable {
+
+    @SerializedName("meta")
+    Meta mMeta;
+
+    @SerializedName("Date")
+    Date mDate;
+
+    @SerializedName("labels")
+    Labels mLabels;
+
+    @SerializedName("series")
+    SeriesList mSeries;
+
+    public Meta getMeta() {
+        return mMeta;
+    }
+
+    public void setMeta(Meta meta) {
+        mMeta = meta;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Labels getLabels() {
+        return mLabels;
+    }
+
+    public void setLabels(Labels labels) {
+        mLabels = labels;
+    }
+
+    public SeriesList getSeries() {
+        return mSeries;
+    }
+
+    public void setSeries(SeriesList series) {
+        mSeries = series;
+    }
+
+    @Override
+    public String toString() {
+        return "FinanceChartCallBack{" +
+                "mMeta=" + mMeta +
+                ", mDate=" + mDate +
+                ", mLabels=" + mLabels +
+                ", mSeries=" + mSeries +
+                '}';
+    }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.mMeta, flags);
+        dest.writeParcelable(this.mDate, flags);
+        dest.writeParcelable(this.mLabels, flags);
+        dest.writeParcelable(this.mSeries, flags);
+    }
+
+    public FinanceChartCallBack() {
+    }
+
+    protected FinanceChartCallBack(Parcel in) {
+        this.mMeta = in.readParcelable(Meta.class.getClassLoader());
+        this.mDate = in.readParcelable(Date.class.getClassLoader());
+        this.mLabels = in.readParcelable(Labels.class.getClassLoader());
+        this.mSeries = in.readParcelable(SeriesList.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<FinanceChartCallBack> CREATOR =
+            new Parcelable.Creator<FinanceChartCallBack>() {
+        @Override
+        public FinanceChartCallBack createFromParcel(Parcel source) {
+            return new FinanceChartCallBack(source);
+        }
+
+        @Override
+        public FinanceChartCallBack[] newArray(int size) {
+            return new FinanceChartCallBack[size];
+        }
+    };
 }
--- app/src/main/java/rajan/udacity/stock/hawk/data/model/financechart/FinanceChartData.java
+++ app/src/main/java/rajan/udacity/stock/hawk/data/model/financechart/FinanceChartData.java
@@ -1,9 +1,62 @@
 package rajan.udacity.stock.hawk.data.model.financechart;
 
+import com.google.gson.annotations.SerializedName;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Rajan Maurya on 31/08/16.
  */
-public class FinanceChartData {
+public class FinanceChartData implements Parcelable {
+
+    @SerializedName("finance_charts_json_callback")
+    FinanceChartCallBack mFinanceChartsJsonCallback;
+
+    public FinanceChartCallBack getFinanceChartsJsonCallback() {
+        return mFinanceChartsJsonCallback;
+    }
+
+    public void setFinanceChartsJsonCallback(FinanceChartCallBack financeChartsJsonCallback) {
+        mFinanceChartsJsonCallback = financeChartsJsonCallback;
+    }
+
+    @Override
+    public String toString() {
+        return "FinanceChartData{" +
+                "mFinanceChartsJsonCallback=" + mFinanceChartsJsonCallback +
+                '}';
+    }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.mFinanceChartsJsonCallback, flags);
+    }
+
+    public FinanceChartData() {
+    }
+
+    protected FinanceChartData(Parcel in) {
+        this.mFinanceChartsJsonCallback =
+                in.readParcelable(FinanceChartCallBack.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<FinanceChartData> CREATOR =
+            new Parcelable.Creator<FinanceChartData>() {
+        @Override
+        public FinanceChartData createFromParcel(Parcel source) {
+            return new FinanceChartData(source);
+        }
+
+        @Override
+        public FinanceChartData[] newArray(int size) {
+            return new FinanceChartData[size];
+        }
+    };
 }
