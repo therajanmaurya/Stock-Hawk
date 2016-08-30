a24c5724cf390b4638b7629afd97bc084e596905
Rajan Maurya <rajanmaurya154@gmail.com>
2016-08-30 18:03:55 +0530

    WidgetRemoteViewsService added

---
+++ app/src/main/java/rajan/udacity/stock/hawk/ui/widget/ListRemoteViewFactory.java
@@ -0,0 +1,68 @@
+package rajan.udacity.stock.hawk.ui.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by Rajan Maurya on 30/08/16.
+ */
+public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory {
+
+
+    private Context mContext;
+    private int mAppWidgetId;
+
+    public ListRemoteViewFactory(Context applicationContext, Intent intent) {
+        mContext = applicationContext;
+        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
