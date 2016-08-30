92162f230a23ae4ba5966964e97a308698effc0d
Rajan Maurya <rajanmaurya154@gmail.com>
2016-08-30 19:53:13 +0530

    Stocks Loading in Widget

--- app/src/main/java/rajan/udacity/stock/hawk/ui/widget/ListRemoteViewFactory.java
+++ app/src/main/java/rajan/udacity/stock/hawk/ui/widget/ListRemoteViewFactory.java
@@ -3,14 +3,18 @@ package rajan.udacity.stock.hawk.ui.widget;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import rajan.udacity.stock.hawk.R;
 import rajan.udacity.stock.hawk.StockHawkApplication;
 import rajan.udacity.stock.hawk.data.model.Quote;
 
@@ -20,31 +24,20 @@ import rajan.udacity.stock.hawk.data.model.Quote;
 public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFactory,
         ListRemoteViewFactoryMvpView {
 
+    private static final String LOG_TAG = ListRemoteViewFactory.class.getSimpleName();
+
     @Inject
     ListRemoteViewFactoryPresenter mListRemoteViewFactoryPresenter;
 
     private Context mContext;
     private int mAppWidgetId;
+    private List<Quote> mQuoteList;
 
     public ListRemoteViewFactory(Context applicationContext, Intent intent) {
         mContext = applicationContext;
         mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
                 AppWidgetManager.INVALID_APPWIDGET_ID);
-    }
-
-    @Override
-    public void showStocks(List<Quote> quoteList) {
-        Toast.makeText(mContext, quoteList.toString() , Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void showStocksEmpty() {
-
-    }
-
-    @Override
-    public void showError() {
-
+        mQuoteList = new ArrayList<>();
     }
 
     @Override
@@ -55,7 +48,45 @@ public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFact
 
     @Override
     public void onDataSetChanged() {
+        mListRemoteViewFactoryPresenter.loadStocks();
+    }
 
+    @Override
+    public RemoteViews getViewAt(int position) {
+
+        Quote quote = mQuoteList.get(position);
+
+        final int itemId = R.layout.appwidget_collection_item;
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), itemId);
+        rv.setTextViewText(R.id.stock_symbol, quote.getMsymbol());
+        rv.setTextViewText(R.id.bid_price, String.valueOf(quote.getBid()));
+        rv.setTextViewText(R.id.change, quote.getChange());
+
+        final Intent viewIntent = new Intent();
+        final Bundle extras = new Bundle();
+        extras.putString("Symbol", quote.getMsymbol());
+        viewIntent.putExtras(extras);
+        rv.setOnClickFillInIntent(R.id.widget_item, viewIntent);
+
+        Log.d(LOG_TAG, quote.getMsymbol());
+
+        return rv;
+    }
+
+    @Override
+    public void showStocks(List<Quote> quoteList) {
+        mQuoteList = quoteList;
+    }
+
+    @Override
+    public void showStocksEmpty() {
+
+    }
+
+    @Override
+    public void showError() {
+        Toast.makeText(mContext, mContext.getString(R.string.error_loading_stocks),
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -65,13 +96,7 @@ public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFact
 
     @Override
     public int getCount() {
-        return 5;
-    }
-
-    @Override
-    public RemoteViews getViewAt(int i) {
-        mListRemoteViewFactoryPresenter.loadStocks();
-        return null;
+        return mQuoteList.size();
     }
 
     @Override
@@ -81,12 +106,12 @@ public class ListRemoteViewFactory implements RemoteViewsService.RemoteViewsFact
 
     @Override
     public int getViewTypeCount() {
-        return 0;
+        return 1;
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
